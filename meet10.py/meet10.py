c3ad581d8467bc96ae210b09d1fad4e07e817fb4
usyami12 <146051216+usyami12@users.noreply.github.com>
2024-06-10 12:30:33 +0800

    10.06.2024

---
+++ meet10.py/meet10.py
@@ -0,0 +1,18 @@
+
+# applikasi penjumlahan sederhana
+
+#memimta input dari pengguna
+num1=input("masukkan angka pertama:")
+num2=input ("masukkan angka kedua: ")
+
+#mengubah input menjadi bilangan bulan
+num1=int(num1)
+num2=int(num2)
+
+#Menjumlahkan dua bilangan 
+result= num1+num2
+#Menampilkan hasil pengumlahan 
+print("Hasil penjumlahan:", result)
+
+
+
