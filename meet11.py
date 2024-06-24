719c5f03fa83952d7f5a7a31f45fcead0f23a042
usyami12 <146051216+usyami12@users.noreply.github.com>
2024-06-24 12:22:02 +0800

    Add files via upload

---
+++ meet11.py
@@ -0,0 +1,13 @@
+# Program untuk menampilkan angka 1 hingga 5 menggunakan looping
+
+# Menggunakan loop for
+print("Menggunakan loop for:")
+for i in range(1, 6):
+    print(i)
+
+# Menggunakan loop while
+print("\nMenggunakan loop while:")
+j = 1
+while j <= 20:
+    print(j)
+    j += 1
